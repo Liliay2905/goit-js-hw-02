6d8d674abe789d28330e1b709f7af8a8e4c0e6cf
Liliya Vitiv <159780612+Liliay2905@users.noreply.github.com>
2024-05-27 00:20:54 +0300

    4

---
+++ js/task-4.js
@@ -0,0 +1,9 @@
+function getShippingCost(country) {
+  switch (country) {
+    case "China":
+      return "Shipping to Australia will cost 170 credits";
+    //   price = 100;
+    //   return `Shipping to ${country} will cost ${price}  credits`;
+    // //   break;
+  }
+}
